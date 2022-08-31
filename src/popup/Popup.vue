d9a34caa9dc80f6d4bc13f6d870cf00d3e8d3b8d
Amirreza Zarkesh <amirreza.zarkesh.23@gmail.com>
2022-08-31 23:47:36 +0430

    feat: :sparkles: add to box word and translation with api

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import { debouncedWatch } from '@vueuse/shared'
-import { Card_GetSuggestions } from '~/repository/Card'
+import { Card_AddWord, Card_GetSuggestions } from '~/repository/Card'
 
 const frontText = ref('')
 const backText = ref('')
+
+const loadingAddWord = ref(false)
 
 debouncedWatch(frontText, () => {
   if (frontText.value) {
@@ -17,6 +19,20 @@ debouncedWatch(frontText, () => {
   }
 }, { debounce: 500 })
 
+function addToBox() {
+  loadingAddWord.value = true
+  Card_AddWord({ back_text: backText.value, front_text: frontText.value })
+    .then(() => {
+      // eslint-disable-next-line no-alert
+      alert(`Added ${backText.value}: ${frontText.value} to box`)
+      frontText.value = ''
+      backText.value = ''
+    })
+    .finally(() => {
+      loadingAddWord.value = false
+    })
+}
+
 </script>
 
 <template>
@@ -29,5 +45,8 @@ debouncedWatch(frontText, () => {
       <BaseInput v-model="frontText" class="w-full" label="Word" />
       <BaseInput v-model="backText" class="w-full mt-4" label="Translation" />
     </div>
+    <BaseButton class="mt-4 w-full" :loading="loadingAddWord" @click="addToBox">
+      Add to box
+    </BaseButton>
   </main>
 </template>
