f59b052d90404e8b9e2d297f35311ffb3d51eeb2
Amirreza Zarkesh <amirreza.zarkesh.23@gmail.com>
2022-08-31 19:18:08 +0430

    feat: :sparkles: get suggestion word

--- src/popup/Popup.vue
+++ src/popup/Popup.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
+import { debouncedWatch } from '@vueuse/shared'
 import { Card_GetSuggestions } from '~/repository/Card'
 
 const frontText = ref('')
+const backText = ref('')
 
-function test() {
-  Card_GetSuggestions({ query: 'test' })
-    .then((response) => {
-      console.log(response)
-    })
-}
+debouncedWatch(frontText, () => {
+  if (frontText.value) {
+    Card_GetSuggestions({ query: frontText.value })
+      .then((response) => {
+        backText.value = response.translate
+      })
+  }
+  else {
+    backText.value = ''
+  }
+}, { debounce: 500 })
 
 </script>
 
 <template>
-  <button @click="test()">
-    test
-  </button>
   <main class="w-[500px] px-4 py-5 text-center">
     <img class="mx-auto" width="128" height="128" src="/assets/logo.png" alt="Logo">
     <h1 class="text-lg font-bold">
@@ -23,6 +27,7 @@ function test() {
     </h1>
     <div class="mt-4">
       <BaseInput v-model="frontText" class="w-full" label="Word" />
+      <BaseInput v-model="backText" class="w-full mt-4" label="Translation" />
     </div>
   </main>
 </template>
