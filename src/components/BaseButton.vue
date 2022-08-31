ff84f34957ee3d9e058daa53820da842a4363e40
Amirreza Zarkesh <amirreza.zarkesh.23@gmail.com>
2022-08-31 23:46:14 +0430

    feat(BaseButton): :sparkles: add BaseButton component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+defineProps<{
+  loading?: boolean
+}>()
+</script>
+
+<template>
+  <button class="bg-primary-200 text-white h-10 px-4 rounded-sm hover:opacity-90">
+    <slot v-if="!loading" />
+    <BaseLoading v-else class="w-6 mx-auto" />
+  </button>
+</template>
