a61170321b9c1686b651b0b7d3723d265cf73ec9
Amirreza Zarkesh <amirreza.zarkesh.23@gmail.com>
2022-09-01 00:06:01 +0430

    style(BaseButton): :lipstick: improve styles

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -5,8 +5,8 @@ defineProps<{
 </script>
 
 <template>
-  <button class="bg-primary-200 text-white h-10 px-4 rounded-sm hover:opacity-90">
+  <button class="bg-primary-200 text-white h-10 px-4 rounded-sm hover:opacity-90 font-bold relative">
     <slot v-if="!loading" />
-    <BaseLoading v-else class="w-6 mx-auto" />
+    <BaseLoading v-if="loading" class="w-6 h-6 mx-auto" />
   </button>
 </template>
